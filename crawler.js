9891cf01358afe98f4819aeb6bf521e531b01e7d
Vizay Soni <vs4vijay@gmail.com>
2017-09-16 01:25:54 +0530

    Add axios

--- crawler.js
+++ crawler.js
@@ -1,18 +1,37 @@
 /**
- * Created by tushar on 13/09/17.
+ * Author: Vijay Soni (vs4vijay@gmail.com)
  */
-
 'use strict'
+
+const axios = require('axios');
+// import axios from 'axios';
+
+const config = {
+  url: 'http://localhost:8080/'
+};
+
+
+
+async function getData(url) {
+  return await axios.get(url)
+    // .then(res => console.log('res', res))
+    // .catch(err => console.log('err', err))
+};
+
+async function crawl() {
+  const response = await axios.get(config.url);
+
+  console.log('data', response.data);
+  console.log(response.status);
+  console.log(response.statusText);
+  console.log(response.headers);
+  console.log(response.config);
+};
+
 
 /**
  * Crawls a website using a start {url}, and returns the lexicographically smallest string.
  * @param url
  * @return {Promise.<string>}
  */
-module.exports = url =>
-  new Promise((resolve, reject) => {
-    /**
-     * TODO: Write your high performance code here.
-     */
-    reject(new Error('NotImplemented'))
-  })
+module.exports = crawl;
