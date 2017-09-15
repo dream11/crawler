fc9c22b8fe8f7daac143023bc478bdd7097d95f0
SGS Venkatesh <venki.iitbbs@gmail.com>
2017-09-16 00:01:33 +0530

    More perf opt

--- crawler.js
+++ crawler.js
@@ -18,21 +18,33 @@ module.exports = function (url) {
 				
 				if (!visitedUrls[pathUrl]) {
 					const h1Matches = html.match(/<h1>(.*?)<\/h1>/g);
-					if (h1Matches) {
-						h1Matches.forEach(thisH1Tag => {
-							const str = thisH1Tag.slice(4, 10);
-							if (str < bestString) { bestString = str; }
-							visitedUrls[pathUrl] = true;
-						});
+					if (h1Matches) { // splitting this for perf
+						if (h1Matches.length === 1) {
+							const code = h1Matches[0].slice(4, 10);
+							if (code < bestString) { bestString = code; }
+						}
+						else if (h1Matches.length === 2) {
+							const code1 = h1Matches[0].slice(4, 10);
+							const code2 = h1Matches[1].slice(4, 10);
+							const localBestString = (code1 < code2 ? code1 : code2);
+							if (localBestString < bestString) { bestString = localBestString }
+						} else {
+							h1Matches.forEach(thisH1Tag => {
+								const str = thisH1Tag.slice(4, 10);
+								if (str < bestString) { bestString = str; }
+							});
+						}
+						visitedUrls[pathUrl] = true;
 					}
 					
-					const hrefMatches = html.match(/href="(.*?)"/g);
-					if (hrefMatches) {
-						hrefMatches.forEach((thisHrefStr, index) => {
-							if (index === 0) { return; }
-							const thisUrl = thisHrefStr.slice(6, -1);
-							!visitedUrls[thisUrl] && fetchUrlAndSaveStrings(thisUrl);
-						});
+					let hrefMatches = html.match(/href="(.*?)"/g);
+					if (hrefMatches && hrefMatches.length > 3) {
+						const url1 = hrefMatches[1].slice(6, -1);
+						const url2 = hrefMatches[2].slice(6, -1);
+						const url3 = hrefMatches[3].slice(6, -1);
+						!visitedUrls[url1] && fetchUrlAndSaveStrings(url1);
+						!visitedUrls[url2] && fetchUrlAndSaveStrings(url2);
+						!visitedUrls[url3] && fetchUrlAndSaveStrings(url3);
 					}
 				}
 				count.callbacksReturned += 1;
