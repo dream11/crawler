6799c3157219ae2333f9a1cec6c20c011a1c48e5
Prasanth <vjprasanth@gmail.com>
2017-09-15 18:40:09 +0530

    second version

--- crawler.py
+++ crawler.py
@@ -1,19 +1,28 @@
 import scrapy
-links = []
-codes_list = []
-class Spider(scrapy.Spider):
-    name = 'spider'
-    start_urls = ['http://localhost:8080']
-    def parse(self, response):
-	for codes in response.css('div.codes > h1 ::text'):    
-	 codes_list.append(codes.extract())
-        for next_page in response.css('div > a.link'):
-         if(next_page.extract() in links):
-    	  yield	  
-	 else: 
-	  links.append(next_page.extract())
-	  yield response.follow(next_page, self.parse)
+from datetime import datetime
+timenow=datetime.now()
+from heapq import heappush, heappop
+code_list = []
 
-    def closed(reason, r2):
-	codes_list.sort()
-	print codes_list.pop(0)
+class Spider(scrapy.Spider):
+	name = 'spider'
+	start_urls = ['http://localhost:8080']
+	custom_settings = {
+        'LOG_ENABLED': 'false',
+		'CONCURRENT_REQUESTS': 4,
+		'CONCURRENT_REQUESTS_PER_DOMAIN': 4
+    }
+	def __init__(self, url=None):
+		self.something = url
+
+	def parse(self, response):
+		local_codes = []
+		for codes in response.css('div.codes > h1 ::text'):
+			heappush(local_codes, codes.extract())
+		yield heappush(code_list, heappop(local_codes))
+		for next_page in response.css('a'):
+			yield response.follow(next_page,callback=self.parse)
+
+	def closed(self, reason):
+		print heappop(code_list)
+		print datetime.now() - timenow
