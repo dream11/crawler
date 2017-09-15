7b5d47ed92628194490be092c43b6d185b3eefe5
Kaushik Wavhal <kaushik.wavhal@games24x7.com>
2017-09-15 17:03:41 +0530

    letsdothis

--- crawler.js
+++ crawler.js
@@ -4,6 +4,19 @@
 
 'use strict'
 
+var request = require('request');
+var FIFO = require("fifo");
+var fifo = FIFO();
+var HTMLParser = require('fast-html-parser');
+var linkStrings = {};
+var bstrings = {};
+var loopIntervalInstance = null;
+var loopInterval = 10;
+var requestCount = 0;
+var responseCount = 0;
+
+/*var responseTimeoutInstance = null;
+var responseTimeoutDuration = 3000;*/
 /**
  * Crawls a website using a start {url}, and returns the lexicographically smallest string.
  * @param url
@@ -11,8 +24,128 @@
  */
 module.exports = url =>
   new Promise((resolve, reject) => {
-    /**
-     * TODO: Write your high performance code here.
-     */
-    reject(new Error('NotImplemented'))
-  })
+	
+  	crawl(url,function(str){
+  		resolve(str);
+  	});
+  	//resolve({});
+    //reject(new Error('NotImplemented'))
+  });
+
+function crawl(url,cb){
+
+	request(url,function(err,res,body){
+		
+		parseBody(res.statusCode,body);
+		loopIntervalInstance = setInterval(function(){
+			loopCrawler(cb);
+		},loopInterval);
+	});
+
+}
+ 
+
+function loopCrawler(cb){
+
+	var popUrl = fifo.shift();
+	if(!popUrl){
+
+		if(requestCount==responseCount){
+			
+			finishFunction(cb);
+		}
+
+	}
+	else{
+		
+		requestCount++;
+		request("http://localhost:8080/"+popUrl,function(err,res,body){
+
+			responseCount++;
+			if(err){
+				return;
+			}
+			parseBody(res.statusCode,body,popUrl);
+
+		});
+
+	}
+	
+}
+
+function finishFunction(cb){
+
+	clearInterval(loopIntervalInstance);
+	cb(findShortestLexString());
+
+}
+
+function parseBody(status, body,popUrl){
+
+
+	if(typeof body == "string"){
+
+		var root = HTMLParser.parse(body);
+		var re = /href=\"\/(.*)\"/g;
+		var linksElem = root.querySelectorAll('.link');
+		for(var i in linksElem){
+			
+			var linksObj = re.exec(linksElem[i].rawAttrs);
+			re.lastIndex = 0;
+			var link = linksObj[1];
+
+			if(!linkStrings[link]){
+				linkStrings[link] = 1;
+				fifo.push(link);
+			}	
+
+		}
+
+		var codes = root.querySelector('.codes');
+		if(codes){
+
+			for(var i in codes.childNodes){
+			var lexString = codes.childNodes[i].childNodes['0'].rawText;
+			
+			if( typeof bstrings[lexString] == "undefined"){				
+				bstrings[lexString] = lexString;
+			}
+			}
+
+		}
+
+	}
+
+	if(status != "200"){
+		
+		fifo.push(popUrl);
+		
+	}
+
+}
+
+function lexcheck(string){
+
+	for(var i=1;i<string.length;i++){
+		if(string.charCodeAt(i)<string.charCodeAt(i-1)){
+			return false;
+		}		
+	}
+	return true;
+
+}
+
+
+function findShortestLexString(){
+	
+	var minString = bstrings[Object.keys(bstrings)[0]];
+	for(var i in bstrings){
+
+		if(bstrings[i]<minString){
+			minString = bstrings[i];
+		}
+
+	}
+	return minString;
+
+}
