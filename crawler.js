53cd34e45c2a99c41115c568d36250255c38ece4
Kaushik Wavhal <kaushik.wavhal@games24x7.com>
2017-09-16 01:20:23 +0530

    opt

--- crawler.js
+++ crawler.js
@@ -7,14 +7,14 @@
 var request = require('request');
 var FIFO = require("fifo");
 var fifo = FIFO();
-var HTMLParser = require('fast-html-parser');
 var linkStrings = {};
 var bstrings = [];
 var loopIntervalInstance = null;
-var loopInterval = 7;
+var loopInterval = 20;
 var requestCount = 0;
 var responseCount = 0;
 var mainUrl = "";
+var TimSort = require('timsort');
 
 /*var responseTimeoutInstancee = null;
 var responseTimeoutDuration = 3000;*/
@@ -38,9 +38,11 @@ function crawl(url,cb){
 	request(url,function(err,res,body){
 		
 		parseBody(res.statusCode,body);
-		loopIntervalInstance = setInterval(function(){
+		loopCrawler(cb);
+
+		/*loopIntervalInstance = setInterval(function(){
 			loopCrawler(cb);
-		},loopInterval);
+		},loopInterval);*/
 	});
 
 }
@@ -49,24 +51,22 @@ function crawl(url,cb){
 function loopCrawler(cb){
 
 	var popUrl = fifo.shift();
-	if(!popUrl){
-
-		if(requestCount==responseCount){
-			
-			finishFunction(cb);
-		}
-
-	}
-	else{
+	if(popUrl){
 		
 		requestCount++;
 		request(mainUrl+ "/"+popUrl,function(err,res,body){
 
 			responseCount++;
+			if(requestCount==responseCount && fifo.length==0){
+			
+				finishFunction(cb);
+			}
 			if(err){
 				return;
 			}
 			parseBody(res.statusCode,body,popUrl);
+			console.log(fifo.length);
+			loopCrawler(cb);
 
 		});
 
@@ -76,7 +76,7 @@ function loopCrawler(cb){
 
 function finishFunction(cb){
 
-	clearInterval(loopIntervalInstance);
+	
 	cb(findShortestLexString());
 
 }
@@ -86,9 +86,74 @@ function parseBody(status, body,popUrl){
 
 	if(typeof body == "string"){
 
+		var re = /href=\"\/(.*)\"/gm;
+		var re1 = /link\" href=\"\/(.*)\">([a-z0-9]{4})</gm;
+
+		var reh1 = /<h1>(.*)<\/h1>/gm;
+
+		//console.time("t5");
+		var t = body.match(re1);
+		var m;
+		while( (m = re.exec(t)) != null){
+			//console.log(m);
+			var s1 = m[1].substr(0,32);
+			//console.log(s1);
+			re.lastIndex = m.index+1;
+
+			if(!linkStrings[s1]){
+				linkStrings[s1] = 1;
+				fifo.push(s1);
+			}			
+
+		}
+		//console.timeEnd("t5");
+		/*
+		
+		console.time("t2");
 		var root = HTMLParser.parse(body);
-		var re = /href=\"\/(.*)\"/g;
-		var linksElem = root.querySelectorAll('.link');
+		console.timeEnd("t2");*/
+
+		//var t1 = body.match(reh1);
+		var n;
+		while( (n = reh1.exec(body)) != null){
+			//console.log(n);
+			var s1 = n[1].substr(0,6);
+			//console.log(s1);
+			reh1.lastIndex = n.index+1;
+			bstrings.push(s1);
+			//fifo.push(s1);
+
+		}
+
+
+		/*console.time("t2");
+		var root = HTMLParser.parse(body);
+		console.timeEnd("t2");
+		var re = /href=\"\/(.*)\"/gm;
+		var re1 = /link\" href=\"\/(.*)\">([a-z0-9]{4})</gm;
+		console.log("=======================================================");
+		console.time("t1");
+		var t = body.match(re1);
+		console.timeEnd("t1");
+		console.log(t);
+		console.log("********************************************************");
+		console.time("t3");
+		var gg = t[0].match(re);
+		re.lastIndex = 0;
+		console.timeEnd("t3");
+		console.log(gg);
+
+		if(t){
+			/*console.log("=======================================================");
+			console.log(t[0] + '\n\n',t[1] );	
+			console.log("=======================================================");*/
+		//}
+		
+
+		//re1.lastIndex = 0;
+		
+
+		/*var linksElem = root.querySelectorAll('.link');
 		for(var i in linksElem){
 			
 			var linksObj = re.exec(linksElem[i].rawAttrs);
@@ -97,12 +162,12 @@ function parseBody(status, body,popUrl){
 
 			if(!linkStrings[link]){
 				linkStrings[link] = 1;
-				fifo.push(link);
+				a3c994551ec4e78d050b2d109fc2c432
 			}	
 
-		}
+		}*/
 
-		var codes = root.querySelector('.codes');
+		/*var codes = root.querySelector('.codes');
 		if(codes){
 
 			for(var i in codes.childNodes){
@@ -113,7 +178,7 @@ function parseBody(status, body,popUrl){
 			
 			}
 
-		}
+		}*/
 
 	}
 
@@ -125,24 +190,12 @@ function parseBody(status, body,popUrl){
 
 }
 
-function lexcheck(string){
-
-	for(var i=1;i<string.length;i++){
-		if(string.charCodeAt(i)<string.charCodeAt(i-1)){
-			return false;
-		}		
-	}
-	return true;
-
-}
-
 
 function findShortestLexString(){
-	bstrings = bstrings.sort(function(a, b){
-	    if(a < b) return -1;
-	    if(a > b) return 1;
-	    return 0;
-	});
+	console.time("sorting");
+	bstrings = bstrings.sort();
+	//TimSort.sort(bstrings);
+	console.timeEnd("sorting");
 	/*
 	var minString = bstrings[Object.keys(bstrings)[0]];
 	for(var i in bstrings){
