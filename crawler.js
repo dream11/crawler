1d38cbb2713f5014f03840190fcb58ef338c5f08
SGS Venkatesh <venki.iitbbs@gmail.com>
2017-09-15 21:18:10 +0530

    minors

--- crawler.js
+++ crawler.js
@@ -4,8 +4,8 @@
 
 'use strict';
 
-module.exports = url =>
-	new Promise((resolve, reject) => {
+module.exports = function (url) {
+	return new Promise(function (resolve) {
 		let request = require('request');
 		let cheerio = require('cheerio');
 		let bestString = "zzzzzz";
@@ -14,25 +14,20 @@ module.exports = url =>
 		
 		function fetchUrlAndSaveStrings(pathUrl) {
 			count.urlsFetched += 1;
-			request(url + pathUrl, function(error, response, html) {
-				if (error) {
-					count.urlsFetched -= 1;
-					return "";
-				}
+			request(url + pathUrl, function (error, response, html) {
+				if (error) { count.urlsFetched -= 1; return; }
 				let $ = cheerio.load(html);
 				
 				if (!visitedUrls.includes(pathUrl)) {
-					$(".codes").children().each((index, elm) => {
-						
+					$(".codes").children().each(function (index, elm) {
 						const str = $(elm).text();
 						if (str < bestString) { bestString = str; }
-						
 						visitedUrls.push(pathUrl);
 					});
 					
 					const links = $("a.link");
 					if (links.length !== 0) {
-						links.each((index, thisAnchorEl) => {
+						links.each(function (index, thisAnchorEl) {
 							const thisUrl = $(thisAnchorEl).attr("href");
 							!visitedUrls.includes(thisUrl) && fetchUrlAndSaveStrings(thisUrl);
 						});
@@ -45,3 +40,4 @@ module.exports = url =>
 		
 		fetchUrlAndSaveStrings('');
 	});
+};
