4ae1a8e6ce6d60375651e696b0f7ecbb38c0944d
SGS Venkatesh <gnanasaivenkatesh.soorisetty@walmart.com>
2017-09-15 20:34:19 +0530

    Solution

--- crawler.js
+++ crawler.js
@@ -1,18 +1,55 @@
 /**
- * Created by tushar on 13/09/17.
+ * Created by sgsvenkatesh on 15/09/17.
  */
 
-'use strict'
+'use strict';
 
-/**
- * Crawls a website using a start {url}, and returns the lexicographically smallest string.
- * @param url
- * @return {Promise.<string>}
- */
 module.exports = url =>
-  new Promise((resolve, reject) => {
-    /**
-     * TODO: Write your high performance code here.
-     */
-    reject(new Error('NotImplemented'))
-  })
+	new Promise((resolve, reject) => {
+		let request = require('request');
+		let cheerio = require('cheerio');
+		let strings = [];
+		let visitedUrls = [];
+		let count = { urlsFetched: 0, callbacksReturned: 0 };
+		
+		function fetchUrlAndSaveStrings(pathUrl) {
+			count.urlsFetched += 1;
+			request(url + pathUrl, function(error, response, html) {
+				if (error) {
+					count.urlsFetched -= 1;
+					return "";
+				}
+				let $ = cheerio.load(html);
+				
+				if (!visitedUrls.includes(pathUrl)) {
+					$(".codes").children().each((index, elm) => {
+						strings.push($(elm).text());
+						visitedUrls.push(pathUrl);
+					});
+					
+					const links = $("a.link");
+					if (links.length !== 0) {
+						links.each((index, thisAnchorEl) => {
+							const thisUrl = $(thisAnchorEl).attr("href");
+							!visitedUrls.includes(thisUrl) && fetchUrlAndSaveStrings(thisUrl);
+						});
+					}
+				}
+				
+				count.callbacksReturned += 1;
+				
+				if (count.urlsFetched === count.callbacksReturned) {
+					let bestStrFound = "";
+					strings.forEach(str => {
+						if (!bestStrFound) { bestStrFound = str; }
+						else if (str < bestStrFound) {
+							bestStrFound = str;
+						}
+					});
+					resolve(bestStrFound);
+				}
+			});
+		}
+		
+		fetchUrlAndSaveStrings('');
+	});
