915d0e535a221aa0064f9031c5043e524e7cae35
Mir ALi <mannanali413@gmail.com>
2017-09-16 08:47:25 +0530

    22secs on local machine

--- crawler.js
+++ crawler.js
@@ -6,11 +6,14 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var rp = require('request-promise');
+var sleep = require('sleep')
 
 var urls = {};
 var baseUrl = '';
 var allSmallest = [];
 var rateLimitedQueue = [];
+var unVisitedUrls = [];
+var randomSleeps = 0;
 /**
  * Crawls a website using a start {url}, and returns the lexicographically smallest string.
  * @param url
@@ -25,39 +28,56 @@ module.exports = url =>
     urls[url] = {
     	sent: false,
     	visited: false,
+    	wasRateLimited: false,
     };
-    requestPromise(url).then(function(){
-    	if(rateLimitedQueue.length > 0){
-    		console.log('I am having some items');
-    	}
-    	resolve(smallestString(allSmallest));
-    }, function(err){
-    	console.log(err);
-    	reject('some in value resolve error');
-    }).catch(function(err){
-    	console.log('catch the error', err);
-    	reject('some error');
-    });
+    const startCrawl = (url) => {
+    	requestPromise(url).then(function(data){
+    		if(unVisitedUrls.length > 0){
+    			startCrawl(unVisitedUrls[0]);
+    		}
+    		else{
+    			console.log(randomSleeps, '----Sleeps');
+    			resolve(smallestString(allSmallest));
+    		}
+    	}, function(err){
+    		if(rateLimitedQueue.length > 0){
+    			var rateLimitedUrl = rateLimitedQueue[0];
+    			randomSleeps++;
+    			startCrawl(rateLimitedUrl);
+    		}
+    	}).catch(function(err){
+    		console.log('catch the error', err);
+    		reject('some error');
+    	});
+    }
+    startCrawl(url);
   });
 
 const smallestString = (stringArray) => stringArray.sort()[0]
 
 const requestPromise = (url) =>  {
-	if(!urls[url].sent){
+	if(!urls[url].sent || (urls[url].sent && !urls[url].visited)){
 		urls[url].sent = true;
 		return new Promise((resolve, reject) => {
 			rp({ uri: url, resolveWithFullResponse: true })
 			.then(function (response) {
 		        if(response.statusCode == 200){
 		        	urls[url].visited = true;
+		        	if(urls[url].wasRateLimited){
+		        		rateLimitedQueue.splice(rateLimitedQueue.indexOf(url), 1);
+		        	}
+		        	unVisitedUrls.splice(unVisitedUrls.indexOf(url), 1);
 		        	resolve(htmlExtractor(response.body));
 		        }
 		    })
 		    .catch(function (err) {
-		       if(err && typeof err === 'object' && err.statusCode == 429){
-		       	urls[url].sent = false;
-		       	rateLimitedQueue.push(url);
-		       	resolve(Promise.resolve());
+		       if(err.statusCode == 429){
+		       	if(!urls[url].wasRateLimited){
+		       		urls[url].sent = true;
+		       		urls[url].wasRateLimited = true;
+		       		rateLimitedQueue.push(url);
+		       	}
+		       	reject('RateLimit Exceeded');
 		       }
 		    });
 		})
@@ -72,7 +92,8 @@ const htmlExtractor = (body) => new Promise( (resolve, reject) => {
 	var newFoundUrls = [];
 	var $ = cheerio.load(body);
 	$('.codes h1').each(function(i, elem) {
-	  stringsInPage.push($(this).text());
+	  // stringsInPage.push($(this).text());
+	  allSmallest.push($(this).text());
 	});
 
 	$('a.link').each(function(i, elem){
@@ -82,16 +103,17 @@ const htmlExtractor = (body) => new Promise( (resolve, reject) => {
 			urls[absoluteUrl] = {
 				sent: false,
 				visited: false,
+				wasRateLimited: false,
 			}
 			newFoundUrls.push(absoluteUrl);
+			unVisitedUrls.push(absoluteUrl);
 		}
 	});
-	console.log(newFoundUrls,'-------');
-	allSmallest.push(smallestString(stringsInPage));
-	 if(newFoundUrls.length > 0){
-		resolve(Promise.all(newFoundUrls.map(requestPromise)));// Map our array of newUrls to an array of page promises
-	}
-	else {
-		resolve(Promise.resolve());
-	}
+	/*var currentSmallest = smallestString(stringsInPage);
+	if(allSmallest.indexOf(currentSmallest) < 0){
+		allSmallest.push(currentSmallest);
+	}*/
+	resolve(Promise.all(newFoundUrls.map(requestPromise)));// Map our array of newUrls to an array of page promises	
+	// resolve(Promise.resolve());
 });
+
