ef2bd12c166b86a7a43be45bdcbb950b3b5919ca
Srijan Gupta <srijan02420@gmail.com>
2017-09-16 15:09:32 +0530

    stupid crawler solution

--- crawler.js
+++ crawler.js
@@ -10,49 +10,44 @@
  * @return {Promise.<string>}
  */
 
-var request = require('request');
-var cheerio = require('cheerio');
+var Crawler = require("crawler");
 
 module.exports = url =>
  new Promise((resolve, reject) => {
 
    var baseURL = url
-   var linksCount = 0
    var bestString = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
    var linksTouched = {}
-   var allLinks = []
 
-   var getBest = async function (url){
-     request(url, function(error, response, body) {
-        if(error) {
+   var c = new Crawler({
+      maxConnections: 70,
+      retryTimeout: 1000,
+      retries: 10,
+      callback: (error, res, done) => {
+        if (error) {
+          return;
+        } else {
+          var $ = res.$;
+          var h1 = $('h1');
+          $(h1).each(function(i, link){
+            if(bestString > $(link).text()){
+              bestString = $(link).text();
+            }
+          });
+
+          var links = $('a');
+          $(links).each(function(i, link){
+            if(!linksTouched[$(link).attr('href')]){
+              c.queue(baseURL + $(link).attr('href'))
+              linksTouched[$(link).attr('href')] = true
+            }
+          });
         }
-        else if(response.statusCode === 200) {
-             var $ = cheerio.load(body);
-             var h1 = $('h1');
-             $(h1).each(function(i, link){
-               if(bestString > $(link).text()){
-                 bestString = $(link).text();
-               }
-             });
-
-             var links = $('a');
-             $(links).each(function(i, link){
-               if(!linksTouched[$(link).attr('href')]){
-                 allLinks.push(baseURL + $(link).attr('href'))
-                 linksTouched[$(link).attr('href')] = true
-               }
-             });
-
-             if(allLinks.length == linksCount){
-               console.log(allLinks.length)
-               resolve(bestString)
-             }
-             else{
-               getBest(allLinks[linksCount])
-               linksCount++
-             }
-         }
-     });
-   }
-   getBest(url)
-})
+        done();
+      },
+    });
+    c.queue(url);
+    c.on("drain", () => {
+      resolve(bestString);
+    });
+  });
