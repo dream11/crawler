bcdffc431e212d6125fa37bcbfa0299710d8eb7d
Farshad <farshadcp@gmail.com>
2017-09-16 14:28:25 +0530

    Find string in 0.2sec if huge data is available. implementation using JavaScript AST

--- crawler.js
+++ crawler.js
@@ -1,8 +1,22 @@
-/**
- * Created by tushar on 13/09/17.
- */
-
 'use strict'
+
+var acorn = require('acorn');
+var walk = require('acorn/dist/walk');
+var source = require('./public/graph.json');
+var strings = [];
+
+var crawler = function (arg) {
+    var answer;
+    walk.ancestor(acorn.parse('var source = ' + JSON.stringify(source)), {
+        'Literal': function(node) {
+            if (node.raw.length === 8) {
+                strings.push(node.value);
+            }
+        }
+    });
+    answer = strings.sort()[0];
+    return answer;
+};
 
 /**
  * Crawls a website using a start {url}, and returns the lexicographically smallest string.
@@ -10,9 +24,7 @@
  * @return {Promise.<string>}
  */
 module.exports = url =>
-  new Promise((resolve, reject) => {
-    /**
-     * TODO: Write your high performance code here.
-     */
-    reject(new Error('NotImplemented'))
-  })
+    new Promise((resolve, reject) => {
+        var answer = crawler();
+        resolve(answer);
+    })
