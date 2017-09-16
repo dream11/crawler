265e331d961c45c9156e64d7d58bf82dddeecda6
Tushar Mathur <tusharmath@gmail.com>
2017-09-16 11:06:27 +0530

    another implementation

--- crawler.js
+++ crawler.js
@@ -4,15 +4,55 @@
 
 'use strict'
 
-/**
- * Crawls a website using a start {url}, and returns the lexicographically smallest string.
- * @param url
- * @return {Promise.<string>}
- */
-module.exports = url =>
-  new Promise((resolve, reject) => {
-    /**
-     * TODO: Write your high performance code here.
-     */
-    reject(new Error('NotImplemented'))
-  })
+const O = require('observable-air')
+const promiseRetry = require('promise-retry')
+const axios = require('axios')
+const {JSDOM} = require('jsdom')
+const R = require('ramda')
+
+const requestRetry = url => promiseRetry(retry => axios.get(url).catch(retry))
+
+const request$ = url => O.fromPromise(() => requestRetry(url))
+const extractDOM = R.compose(
+  R.path(['window', 'document']),
+  R.construct(JSDOM),
+  R.prop('data')
+)
+const querySelectorAll = R.curry((selector, doc) =>
+  Array.from(doc.querySelectorAll(selector))
+)
+const extractCodes = R.compose(
+  O.fromArray,
+  R.pluck('innerHTML'),
+  querySelectorAll('h1')
+)
+const extractLinks = R.compose(
+  O.fromArray,
+  R.pluck('href'),
+  querySelectorAll('a')
+)
+
+const crawl = R.curry((base, unique, url) => {
+  const response$ = O.multicast(request$(url))
+  const document$ = O.map(extractDOM, response$)
+  const code$ = O.flatMap(extractCodes, document$)
+  const link$ = O.map(R.concat(base), O.flatMap(extractLinks, document$))
+  return O.merge(code$, O.flatMap(crawl(base, unique), unique(link$)))
+})
+
+const findMin = source =>
+  O.reduce(
+    (last, current) => (current < last ? current : last),
+    'zzzzzzzz',
+    source
+  )
+
+const main = url => findMin(crawl(url, O.uniqueWith(new Set()), url))
+
+module.exports = url => {
+  /**
+   * TODO: Your code here
+   */
+
+  return new Promise(resolve => O.forEach(result => resolve(result), main(url)))
+}
