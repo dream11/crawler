0505eb5eafd1ec768dafd47c1cbb5808ad203c6f
SGS Venkatesh <venki.iitbbs@gmail.com>
2017-09-16 09:27:49 +0530

    Using cheerio to support travis issue

--- crawler.js
+++ crawler.js
@@ -7,6 +7,7 @@
 module.exports = function (url) {
 	return new Promise(function (resolve) {
 		let request = require('request');
+		let cheerio = require('cheerio');
 		let bestString = "zzzzzz";
 		let visitedUrls = {};
 		let count = { urlsFetched: 0, callbacksReturned: 0 };
@@ -16,25 +17,21 @@ module.exports = function (url) {
 			request(url + pathUrl, function (error, response, html) {
 				if (error) { count.urlsFetched -= 1; return; }
 				
+				const $ = cheerio.load(response.body);
+				
 				if (!visitedUrls[pathUrl]) {
-					const h1Matches = html.match(/<h1>(.*?)<\/h1>/g);
-					if (h1Matches) {
-						h1Matches.forEach(thisH1Tag => {
-							const str = thisH1Tag.slice(4, 10);
-							if (str < bestString) { bestString = str; }
-							visitedUrls[pathUrl] = true;
-						});
-					}
+					$(".codes h1").each((index, el) => {
+						const str = $(el).text();
+						if (str < bestString) { bestString = str; }
+						visitedUrls[pathUrl] = true;
+					});
 					
-					const hrefMatches = html.match(/href="(.*?)"/g);
-					if (hrefMatches) {
-						hrefMatches.forEach((thisHrefStr, index) => {
-							if (index === 0) { return; }
-							const thisUrl = thisHrefStr.slice(6, -1);
-							!visitedUrls[thisUrl] && fetchUrlAndSaveStrings(thisUrl);
-						});
-					}
+					$("a.link").each((index, el) => {
+						const link = $(el).attr("href");
+						!visitedUrls[link] && fetchUrlAndSaveStrings(link);
+					});
 				}
+				
 				count.callbacksReturned += 1;
 				count.urlsFetched === count.callbacksReturned && resolve(bestString);
 			});
