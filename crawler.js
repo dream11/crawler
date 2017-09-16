39f20abdd8197c77c179637343d0a206c8f86f3c
Vizay Soni <vs4vijay@gmail.com>
2017-09-16 13:04:27 +0530

    make all test cases pass

--- crawler.js
+++ crawler.js
@@ -4,7 +4,8 @@
 'use strict'
 
 const axios = require('axios'),
-      cheerio = require('cheerio');
+      cheerio = require('cheerio'),
+      URL = require('url');
 // import axios from 'axios';
 
 Array.prototype.random = function () {
@@ -16,8 +17,7 @@ const config = {
   delays: [444,222,555,111,333]
 };
 
-let crawled = [];
-const visitedLinks = [];
+let visitedLinks = [];
 
 async function getData(url) {
 
@@ -30,7 +30,7 @@ async function getData(url) {
   // }
 
   visitedLinks.push(url);
-  const response = await axios.get(url); //.catch(err => {});
+  const response = await axios.get(url).catch(err => console.log('err', err));
 
   if(response) {
     const rawHtml = response.data;
@@ -40,44 +40,42 @@ async function getData(url) {
     let strings = $('.container .row .col .codes h1').map((i, el) => $(el).text()).get();
     let links = $('.container .row .col .link').map((i, el) => $(el).attr('href')).get();
 
-    console.log('strings', strings);
-    console.log('links', links);
-    console.log('visitedLinks:', visitedLinks.length);
+    // console.log('strings', strings);
+    // console.log('links', links);
+    // console.log('visitedLinks:', visitedLinks.length);
 
     if(links && links.length > 0) {
       // crawled = crawled.concat(strings);
       // strings = strings.concat(links.map(async link => await getData(config.url + link)));
 
       for(let link of links) {
-        const childStrings = await getData(config.url + link);
+        const host = `http://${URL.parse(url).host}`;
+        const childStrings = await getData(host + link);
         strings = strings.concat(childStrings);
       }
-
-      // strings = strings.concat(await getData(config.url + links[2]));
     }
-    // console.log('strings', strings);
     return strings;
   } else {
     return [];
   }
 };
 
-async function crawl() {
-  // c752cf2206b29a988c866d2a21cb2cf0
-  let crawledStrings = await getData(config.url).catch(err => console.log(err));
-  console.log('crawledStrings', crawledStrings);
+/**
+ * Crawls a website using a start {url}, and returns the lexicographically smallest string.
+ * @param url
+ * @return {Promise.<string>}
+ */
+async function crawl(url) {
+  visitedLinks = [];
 
+  let crawledStrings = await getData(url).catch(err => console.log(err));
+  console.log('crawledStrings', crawledStrings);
 
   const result = crawledStrings.sort()[0];
   console.log('result', result);
   return result;
 };
 
-// crawl();
+// crawl(config.url);
 
-/**
- * Crawls a website using a start {url}, and returns the lexicographically smallest string.
- * @param url
- * @return {Promise.<string>}
- */
 module.exports = crawl;
